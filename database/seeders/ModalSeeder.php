364c1a9051281f64a0352da8803468c4297d3ae5
Fendika Edo Ferdinata <fendikaedo@gmail.com>
2023-12-09 23:54:36 +0700

    Create Seeder Table

---
+++ database/seeders/ModalSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ModalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('modal')->insert([
+            ['status_modal' => 'PMDN'],
+            ['status_modal' => 'Bukan PMA/PMDN'],
+            ['status_modal' => 'PMA'],
+        ]);
+    }
+}
---
+++ database/seeders/Skala_UsahaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Skala_UsahaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
